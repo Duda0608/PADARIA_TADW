45d8240ff892e809e0387bf23f8e1e58393b357b
Nalaura2002 <anaalmeidaaam@gmail.com>
2025-05-26 15:49:39 -0300

    index e formusu
    add index e form usuario

---
+++ formusuario.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="salvarUsuario.php" method="post">
+        Nome: <br>
+        <input type="text" name="nome"> <br><br>
+        Cpf: <br>
+        <input type="text" name="cpf"> <br><br>
+        Telefone: <br>
+        <input type="text" name="telefone"> <br><br>
+        Endereço: <br>
+        <input type="text" name="endereco"> <br><br>
+        Gmail: <br>
+        <input type="text" name="gmail"> <br><br>
+        Senha: <br>
+        <input type="text" name="senha"> <br><br>
+
+        <input type="submit" value="Cadastrar">
+    </form>
+</body>
+</html>
